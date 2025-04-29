7925f95011591bb1555074c27cea32112825e0e5
Haokun.Feng <haokun.fung@gmail.com>
2025-04-29 06:52:41 +0000

    update

--- rl_utils.py
+++ rl_utils.py
@@ -3,6 +3,13 @@ import numpy as np
 import torch
 import collections
 import random
+import gym
+import time
+import os
+import math
+from moviepy import ImageSequenceClip, clips_array
+import json
+import matplotlib.pyplot as plt
 
 class ReplayBuffer:
     def __init__(self, capacity):
@@ -19,7 +26,9 @@ class ReplayBuffer:
     def size(self): 
         return len(self.buffer)
 
-
+# ---------------------------------------------------------------------------------------------
+# Functions to train on-policy and off-policy agents
+# ---------------------------------------------------------------------------------------------
 
 def train_on_policy_agent(env, agent, num_episodes):
     return_list = []
@@ -100,6 +109,9 @@ def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size
                 pbar.update(1)
     return return_list
 
+# ---------------------------------------------------------------------------------------------
+# Functions to compute advantage and moving average
+# ---------------------------------------------------------------------------------------------
 
 def compute_advantage(gamma, lmbda, td_error):
     td_error = td_error.detach().numpy()
@@ -119,3 +131,96 @@ def moving_average(a, window_size):
     begin = np.cumsum(a[:window_size-1])[::2] / r
     end = (np.cumsum(a[:-window_size:-1])[::2] / r)[::-1]
     return np.concatenate((begin, middle, end))
+
+# ---------------------------------------------------------------------------------------------
+# Functions to save and load models
+# ---------------------------------------------------------------------------------------------
+def save_model(agent, save_path):
+    os.makedirs(os.path.dirname(save_path))
+
+
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------------
+# Functions to display and record agent performance
+# ---------------------------------------------------------------------------------------------------------------
+def watch_agent(env_name, agent, device, sleep_time=0.01, num_episodes=1):
+    env_fn = lambda: gym.make(env_name, render_mode='human')
+    for i in range(num_episodes):
+        env = env_fn()
+        reset_output = env.reset()
+        state = reset_output[0] if isinstance(reset_output, tuple) else reset_output
+        done = False
+        while not done:
+            env.render()
+            with torch.no_grad():
+                action = agent.take_action(state)
+            
+            next_state, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            state = next_state
+
+            time.sleep(sleep_time)
+        env.close()
+
+
+def record_one_episode(env_name, agent, device, save_path='./video', filename='agent_play', fps=30):
+    os.makedirs(save_path, exist_ok=True)
+    env_fn = lambda: gym.make(env_name, render_mode='rgb_array')
+    env = env_fn()
+    frames = []
+
+    reset_output = env.reset()
+    state = reset_output[0] if isinstance(reset_output, tuple) else reset_output
+    done = False
+
+    while not done:
+        frame = env.render()
+        frames.append(frame)
+        with torch.no_grad():
+            action = agent.take_action(state)
+        next_state, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
+        state = next_state
+
+    env.close()
+    clip = ImageSequenceClip(frames, fps=fps)
+    clip.write_gif(os.path.join(save_path, f'{filename}.gif'), fps=fps)
+
+def record_multiple_episodes(env_name, agent, device, num_episodes=10, videos_per_row=5, save_path='./video', filename='agent_multi_play', fps=30):
+    os.makedirs(save_path, exist_ok=True)
+    env_fn = lambda: gym.make(env_name, render_mode='rgb_array')
+    all_clips = []
+    for ep in range(num_episodes):
+        env = env_fn()
+        frames = []
+
+        reset_output = env.reset()
+        state = reset_output[0] if isinstance(reset_output, tuple) else reset_output
+        done = False
+
+        while not done:
+            frame = env.render()
+            frames.append(frame)
+            with torch.no_grad():
+                action = agent.take_action(state)
+            next_state, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            state = next_state
+        
+        env.close()
+
+        clip = ImageSequenceClip(frames, fps=fps)
+        all_clips.append(clip)
+    
+    rows = []
+    for i in range(0, num_episodes, videos_per_row):
+        row = all_clips[i:i+videos_per_row]
+        rows.append(row)
+    
+    final_clip = clips_array(rows)
+    final_clip.write_gif(os.path.join(save_path, f'{filename}.gif'), fps=fps)
+
+
